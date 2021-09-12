d42a752657a94c8fc8f1bf93e5eaa9a7ac612991
CTRLALTPSC <65181873+CTRLALTPSC@users.noreply.github.com>
2021-09-11 23:10:55 -0400

    9.11

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');                   // sets color on msgs to group
 const debug = require('debug')('app')            // pass in file or section of code that we are in
 const morgan = require('morgan');
 const path = require('path')
+const sessions = require('./src/data/sessions.json');
 
 const PORT = process.env.PORT || 3000;        
 const app = express();      
@@ -34,17 +35,15 @@ app.use('/sessions', sessionsRouter);
 // use middleware (use), everything that goes to sessions, implement sessionrouter [ holds all code necessary to deal with sessions route]
 
 sessionsRouter.route('/').get((req,res) => {
-    res.render('sessions', {sessions: [  // pass in object, pass in an array of sessions: title and description  
-        {title: 'Session 1', description: 'this is sessions 1'},
-        {title: 'Session 2', description: 'this is sessions 2'},
-        {title: 'Session 3', description: 'this is sessions 3'},
-        {title: 'Session 4', description: 'this is sessions 4'},
-        ],  // when sessions is rendering, object is getting passed in that which has sessions data in it 
+    res.render('sessions', {  // pass in object, pass in an array of sessions: title and description {deleted} -> pass sessions in on object instead
+        sessions, // looping over sessions.map, pulling all sesions out of .json file for information under "learn more" 
+                                // when sessions is rendering, object is getting passed in that which has sessions data in it 
     });                
 });
 
-sessionsRouter.route('/1').get((req,res) => {
-    res.send('hello single sessions')
+sessionsRouter.route('/:id').get((req,res) => {         // :id / whatever comes after will be passed into this router function
+    const id = req.params.id;       // http://localhost:4000/sessions/sdfghdsfhh--> "hello single sessions sdfhsdkg" onto page
+    res.send('hello single session ' + id);
 });
 
 app.get('/', (req, res) => { 
